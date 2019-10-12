50cc5199a73152bf73970fa13dfe2e13f3ef09dc
Dave Conlan <52613691+dconlan1@users.noreply.github.com>
2019-10-11 21:37:36 -0700

    Add files via upload
    uploading structure of all files

---
+++ game.js
@@ -0,0 +1,37 @@
+// first create a random number
+// then capture what letter user is thinking maybe do a prompt or alert?
+// compare the user's number guess against the random number created. 
+// if they match, then return "you win"
+// also, record a win
+// create the following vars : wins, losses, guessLeft, guessesSoFar, userGuess, computerGuess.
+
+
+
+var my_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var ri = Math.floor(Math.random() * my_array.length);
+document.write(my_array[ri]);
+
+var wins = ""
+var losses = ""
+var guessesLeft = ""
+var userGuess = document.getElementById()
+
+
+var wins = ""
+var losses = ""
+var guessesLeft = ""
+var userGuess = document.getElementById()
+
+
+var my_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var ri = Math.floor(Math.random() * my_array.length);
+document.write(my_array[ri]);
+
+
+//  now let's get the loop built in order to create the random number. 
+
+var i;
+
+for (i = 0; i < letters.length; i++) {
+
+}
